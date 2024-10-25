7154578733370a328fae33347829baecf1596744
kshipraDewat <kshipradewat66@gmail.com>
2024-10-25 12:21:05 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
    <div>
-    The Kitchen
+    <NavBar/>
+
    </div>
   )
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,34 @@
+import { Heart, Home, Search } from 'lucide-react';
+import { Link } from 'react-router-dom'
+
+
+const NavBar = () => {
+
+  
+  return (
+    <div className='flex justify-between items-center py-5 px-10 shadow md:shadow-none items-center'>
+      <div>
+        <h1 className='text-2xl font-bold'>The<span className='text-green-400'>Kitchen</span></h1>
+      </div>
+       <form  className='hidden md:flex md:w-[25vw] ms-28 border bg-white rounded-lg flex items-center px-2 py-1 '>
+        
+         <Search className='stroke-gray-400 sm:hidden lg:block'/>
+        <input
+          type="text"
+          name="search"
+          placeholder=" Enter Items..."
+          className='outline-none p-2 w-[20vw] rounded-lg' 
+        />
+        
+      </form>
+      <div className='flex gap-5 md:gap-16 '>
+
+        <Link to={'/'} className='font-semibold hover:text-green-400 flex gap-1 items-center'> <Home size={20}/> Home</Link>
+        <Link to={'/favourites '} className='font-semibold hover:text-green-400 flex gap-1 items-center '> <Heart size={20}/>  Favourites</Link>
+      </div>
+    </div>
+  )
+}
+
+
+export default NavBar;
