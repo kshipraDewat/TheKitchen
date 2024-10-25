2101d82a261654817153d95ae2966722d44ecdab
kshipraDewat <kshipradewat66@gmail.com>
2024-10-25 12:34:31 +0530

    Routs setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css'
 import NavBar from './components/NavBar'
+import Details from './pages/Details'
+import Favourites from './pages/Favourites'
 import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
-   <div>
-    <NavBar/>
-    <Home/>
-
-   </div>
+    <div className='h-[96vh]' >
+      <NavBar/>
+      
+         <Routes>
+          <Route path='/' element={<Home/>}/> 
+          <Route path='/favourites' element={<Favourites/>}/>
+          <Route path='/recipe-item/:id' element={<Details/>}/>
+         </Routes>
+    </div>
   )
 }
 
